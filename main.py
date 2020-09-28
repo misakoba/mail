7124589c7c27209cec83bfc5ee584fa986729bbf
Dan Garubba <dan.garubba@gmail.com>
2020-09-27 21:30:22 -0700

    Send 500 error on reCAPTCHA server error.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """A Flask app implementing the mailing backend for misakoba.github.io."""
 
+import http
 import os
 
 import flask
@@ -13,12 +14,18 @@ flask_cors.CORS(app)
 @app.route('/send', methods=['POST'])
 def send():
     """Serves the '/send' endpoint for sending messages."""
-    requests.post(
+    response = requests.post(
         'https://www.google.com/recaptcha/api/siteverify',
         params={
             'secret': os.environ['RECAPTCHA_SECRET'],
             'response': flask.request.args['recaptcha_response'],
         })
+
+    try:
+        response.raise_for_status()
+    except requests.HTTPError:
+        flask.abort(http.HTTPStatus.INTERNAL_SERVER_ERROR,
+                    b'Error in communicating with reCAPTCHA server.')
 
     return 'Successfully validated message request.'
 
--- main_test.py
+++ main_test.py
@@ -7,6 +7,7 @@ import pytest
 # pylint: disable=unused-import
 import pytest_subtests  # type: ignore # noqa: F401
 # pylint: enable=unused-import
+import requests
 
 import main
 
@@ -46,6 +47,32 @@ def test_send_wrong_method(client):  # pylint: disable=redefined-outer-name
     """Tests a 403 error returned when accessing send with a bad method."""
     assert (client.get('/send').status_code ==
             http.HTTPStatus.METHOD_NOT_ALLOWED)
+
+
+def test_send_recaptcha_request_failed(
+        client):  # pylint: disable=redefined-outer-name
+    """Tests a 500 error returned when accessing send with a bad method."""
+    mock_recaptcha_response = mock.create_autospec(requests.Response,
+                                                   instance=True)
+    mock_recaptcha_response.raise_for_status.side_effect = requests.HTTPError(
+        'Bad request.')
+
+    with mock.patch.dict('os.environ',
+                         {'RECAPTCHA_SECRET': 'my_secret'}):
+        with mock.patch('requests.post', autospec=True) as mock_post:
+            mock_raise_for_status = mock_post.return_value.raise_for_status
+            mock_raise_for_status.side_effect = requests.HTTPError(
+                'Bad request.')
+            response = client.post(
+                f'/send?recaptcha_response=my_token')
+            mock_post.assert_called_once_with(
+                'https://www.google.com/recaptcha/api/siteverify',
+                params={'secret': 'my_secret',
+                        'response': 'my_token'})
+            assert (response.status_code ==
+                    http.HTTPStatus.INTERNAL_SERVER_ERROR)
+            assert (b'Error in communicating with reCAPTCHA server.' in
+                    response.data)
 
 
 if __name__ == '__main__':
